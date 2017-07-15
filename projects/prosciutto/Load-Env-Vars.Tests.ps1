158cb0cd68a653c5e4045044ee8e42c534a39bb0
Rafael Fiume <rafael.fiume@yahoo.it>
2017-07-15 19:54:25 +0100

    #1 : Adds script to load environment variables
    @rafaelfiume

---
+++ projects/prosciutto/Load-Env-Vars.Tests.ps1
@@ -0,0 +1,60 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "Load-Env-Vars" {
+    Mock Set-Var-In-Dev-Environment
+    It "should really set environment variables defined for a project" {
+        Load-Env-Vars
+        Assert-MockCalled Set-Var-In-Dev-Environment -Scope It -Times 1 -ParameterFilter { $Key -eq "SUPPLIER_STAGING_URL"}
+        Assert-MockCalled Set-Var-In-Dev-Environment -Scope It -Times 1 -ParameterFilter { $Value -eq "http://sheltered-river-1312.herokuapp.com/salume/supplier"}
+    }
+}
+
+Describe "Load-Env-Vars" {
+    $firstKey = "FIRST_ENV_VAR"
+    $firstValue = "C:\the\first"
+    $secondkey = "SECOND_ENV_VAR"
+    $secondValue = "the_second"
+
+    $projectPath = "C:\a\path\to\a\project"
+
+    Mock Get-Vars-File { return $projectPath }
+    Mock Read-Vars { return @{ $firstKey = $firstValue; $secondKey = $secondValue} }
+    Mock Set-Var-In-Dev-Environment
+
+    It "should set environment variables defined for a project" {
+        Load-Env-Vars
+
+        Assert-MockCalled Read-Vars -Scope It -Times 1 -ParameterFilter { $Path -eq $projectPath}
+
+        Assert-MockCalled Set-Var-In-Dev-Environment -Scope It -Times 1 -ParameterFilter { $Key -eq $firstKey}
+        Assert-MockCalled Set-Var-In-Dev-Environment -Scope It -Times 1 -ParameterFilter { $Value -eq $firstValue}
+        Assert-MockCalled Set-Var-In-Dev-Environment -Scope It -Times 1 -ParameterFilter { $Key -eq $secondKey}
+        Assert-MockCalled Set-Var-In-Dev-Environment -Scope It -Times 1 -ParameterFilter { $Value -eq $secondValue}
+    }
+}
+
+Describe "Read-Vars" {
+    It "should return as map the vars defined in a file" {
+        $varsFile = "TestDrive:\env.vars"
+        Add-Content $varsFile "FIRST_ENV_VAR=C:\the\first"
+        Add-Content $varsFile "SECOND_ENV_VAR=the_second"
+
+        $vars = Read-Vars -Path $varsFile
+
+        $vars."FIRST_ENV_VAR" | Should BeExactly "C:\the\first"
+        $vars."SECOND_ENV_VAR" | Should BeExactly "the_second"
+    }
+}
+
+Describe "Get-Vars-File" {
+    It "should return the path to the env.vars file of a project" {
+        $project = "prosciutto"
+
+        $pathToVarsFile = Get-Vars-File -Project $project
+
+        $expectedPath = Join-Path -Path $PSScriptRoot -ChildPath "env.vars"
+        $pathToVarsFile | Should be $expectedPath
+    }
+}
---
+++ projects/prosciutto/Load-Env-Vars.ps1
@@ -0,0 +1,30 @@
+﻿function Load-Env-Vars {
+    Write-Host "Loading prosciutto environment variables..."
+    $varsFilePath = Get-Vars-File -Project "prosciutto"
+    $vars = Read-Vars -Path $varsFilePath
+    foreach ($key in $vars.Keys) {
+        $val = $vars.$key
+        Set-Var-In-Dev-Environment -Key $key -Value $val
+        Write-Host "Added $($key) = $($val)"
+    }
+}
+
+function Read-Vars([string] $Path) {
+    $vars = @{}
+    Get-Content $path | ForEach-Object {$key,$value = $_.Split("="); $vars.Add($key, $value) }
+    $vars
+}
+
+function Get-Vars-File([string] $Project) {
+    Join-Path -Path (Get-Dir -Project $Project) -ChildPath "env.vars"
+}
+
+function Set-Var-In-Dev-Environment([string] $Key, [string] $Value) {
+    [System.Environment]::SetEnvironmentVariable($Key, $Value, "User")
+}
+
+function Get-Dir([string] $Project) {
+    $PSScriptRoot
+}
+
+Load-Env-Vars
